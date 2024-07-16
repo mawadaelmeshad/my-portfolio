df130e5ff175eaa964299c44d25c715fd98afdf5
mawadaelmeshad <mawadahassan21@gmail.com>
2024-07-16 15:16:13 +0300

    about me updated

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ function About(){
             <div className="contain-about">
                 <div className="contain">
                     <div className="contain-p">
-                        <p><span className="dash">-</span> I am a passionate junior computer science student with a deep love for problem-solving and a strong interest in front-end development.</p>
+                        <p><span className="dash">-</span> I am a passionate girl with a deep love for problem-solving and a strong interest in front-end development.</p>
                             <p id="removed"><span className="dash">-</span> My journey in the field of technology has been a continuous exploration of learning and gaining valuable experiences. I thrive on the challenges of creating intuitive user interfaces and solving real-world problems through code.</p>  
                             <p><span className="dash">-</span> With an insatiable curiosity and a commitment to excellence, I am dedicated to furthering my knowledge, embracing new technologies, and making a meaningful impact in the world of web development. I'm excited to continue this incredible journey and see where it takes me.</p>
                     </div>
@@ -34,7 +34,7 @@ function About(){
                 </div>
                 <img src={flower1} alt="flower" />
                    {/* <div className="myphoto">
-                   <    img src={myphoto} alt="myphoto" />
+                   <    img src={myphoto} alt="myphoto" />I am a passionate professional with a deep love for problem-solving and a strong interest in front-end development. My journey in the field of technology has been a continuous exploration of learning and gaining valuable experiences. I thrive on the challenges of creating intuitive user interfaces and solving real-world problems through code.
                    </div> */}
 
             </div>
